188fc38757e9abeb44fa255855b67d1816ca9d2e
alex <alex@nabu.be>
2015-03-24 14:12:35 +0100

    Fixed test case

--- src/test/java/be/nabu/glue/TestScripts.java
+++ src/test/java/be/nabu/glue/TestScripts.java
@@ -16,7 +16,7 @@ import be.nabu.glue.repositories.TargetedScriptRepository;
 
 public class TestScripts extends TestCase {
 	public void test() throws IOException, ParseException, URISyntaxException {
-		ScriptRepository repository = new TargetedScriptRepository(null, new URI("classpath:/"), null, new GlueParserProvider(), Charset.forName("UTF-8"), "glue");
+		ScriptRepository repository = new TargetedScriptRepository(null, new URI("classpath:/scripts"), null, new GlueParserProvider(), Charset.forName("UTF-8"), "glue");
 		Script script = repository.getScript("testAll");
 		ScriptRuntime runtime = new ScriptRuntime(
 			script,
